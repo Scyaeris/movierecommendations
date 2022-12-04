e97ebd87631868127dc6361ddd1294d1710b7b33
Ryan Ho <apexial@aol.com>
2022-12-04 06:30:57 -0500

    welcome to frontend

---
+++ obs/app.py
@@ -0,0 +1,97 @@
+#Import modules
+from flask import Flask, redirect, render_template, request, flash, url_for
+import knn_model
+import http.client
+import random
+#Initiate flask app and integrate API source key
+app = Flask(__name__)
+
+#Define route for home index
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/knn")
+def knn():
+    return render_template("knn.html")
+
+@app.route("/nn")
+def nn():
+    flash("Search to begin.")
+    return render_template("nn.html")
+
+@app.route("/predictKNN", methods = ['POST'])
+def predictKNN():
+
+    data = request.form.get("data")
+    x = data.find("/")
+
+    if len(data) == 0:
+        flash("You can not input a blank string.", 'error')
+        return redirect(url_for('knn'))
+    elif "/" not in data:
+        result = knn_model.recommend(data, knn_model.getCSR(), knn_model.getModel(), knn_model.getMovieToIndex(), 5)
+        if result == None:
+            flash("This movie does not exist within the dataset. Please try a different movie.", 'error')
+            return redirect(url_for('knn'))
+    elif "/" in data and data[x + 1:].isdigit():
+        title = data[:x]
+        n = data[x + 1:]
+        x = int(n)
+        if x > 10:
+            flash("Your query size was too large. Defaulting to size 5.", 'warning')
+            result = knn_model.recommend(title, knn_model.getCSR(), knn_model.getModel(), knn_model.getMovieToIndex(), 5)
+            return render_template('knn.html', result = result)
+        else:
+            result = knn_model.recommend(title, knn_model.getCSR(), knn_model.getModel(), knn_model.getMovieToIndex(), int(n))
+        if result == None:
+            flash("This movie does not exist within the dataset. Please try a different movie.", 'error')
+            return redirect(url_for('knn'))
+    else:
+        flash("Something went wrong. Please try again.", 'error')
+        return redirect(url_for('knn'))
+
+    return render_template('knn.html', result = result)
+
+@app.route("/predictNN", methods = ['POST'])
+def predictNN():
+
+    data = request.form.get("data")
+
+    if len(data) == 0:
+        flash("You can not input a blank string.")
+        return render_template('nn.html')
+    elif len(data) > 0:
+        result = knn_model.recommend(data, knn_model.getCSR(), knn_model.getModel(), knn_model.getMovieToIndex(), 12)
+        if result == None:
+            flash("This movie does not exist within the dataset.")
+            return render_template('nn.html')
+    else:
+        flash("Something went wrong. Please try again.")
+        return render_template('nn.html')
+
+    image_urls = []
+    randInt = []
+    for item in result:
+        item_parse = item.replace(" ", '')
+        conn = http.client.HTTPSConnection("bing-image-search1.p.rapidapi.com")
+        headers = {
+            'X-RapidAPI-Key': "7e0ff16003msh9fd9042dd24b37ep13cf04jsned3e016768fa",
+            'X-RapidAPI-Host': "bing-image-search1.p.rapidapi.com"
+        }
+        conn.request("GET", "/images/search?q=" + item_parse + "landscape%20imdb%20cover&count=1", headers=headers)
+        res = conn.getresponse()
+        data = res.read().decode("utf-8")
+        start = data.find("thumbnailUrl") + 16
+        stop = data.find('pid=Api"') + 7
+        url = data[start:stop]
+        print(len(image_urls))
+
+        randInt.append(random.randint(1,9))
+        image_urls.append(url)
+    return render_template('nn.html', result = result, images = image_urls, random = randInt)
+
+#Enable debug mode for development purposes
+if __name__ == "__main__":
+    app.run(debug=True)
+app.secret_key = 'WDNAJHDKUAHJSKDJKWAJOIS'
---
+++ obs/nn.py
@@ -0,0 +1,135 @@
+
+import pandas as pd
+import torch
+import torch.nn as nn
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#import the ratings and movies data set
+ratings = pd.read_csv('ratings.csv', encoding = 'utf-8')
+movies = pd.read_csv('movies.csv', encoding = 'utf-8')
+
+#join ratings and movies data set
+data = pd.merge(movies, ratings, on='movieId')
+
+#visualizing the ratings
+sns.set_style('ticks')
+sns.catplot(x='rating', data=data, kind='count', aspect=2.5)
+plt.show()
+
+#Split data into training, validation and test set
+from sklearn.model_selection import train_test_split
+training_data, test_data = train_test_split(data, test_size=0.2)
+training_data, validation_data = train_test_split(training_data, test_size=0.2)
+
+#create the dataloaders
+from torch.utils.data import DataLoader, TensorDataset
+
+#create Tensor datasets from Partioned data
+
+train_tensor_ds = TensorDataset(torch.FloatTensor(training_data.values), torch.LongTensor(training_data.values))
+valid_tensor_ds = TensorDataset(torch.FloatTensor(validation_data.values), torch.LongTensor(validation_data.values))
+test_tensor_ds = TensorDataset(torch.FloatTensor(test_data.values), torch.LongTensor(test_data.values))
+
+#create data loaders from Tensor datasets
+
+train_loader = DataLoader(dataset=train_tensor_ds, batch_size=32, shuffle=True)
+validation_loader = DataLoader(dataset=valid_tensor_ds, batch_size=32, shuffle=True)
+test_loader = DataLoader(dataset=test_tensor_ds, batch_size=32, shuffle=True)
+
+#Define a Convolutional Neural Network
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+
+        #Define 2D Convolution Layer
+        self.conv_layer = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=(3,3), padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2))
+        #Fully Connected Layer
+        self.fc_layer = nn.Sequential(
+            nn.Linear(20*20*32, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1),
+            nn.Sigmoid()
+            )
+
+    #Define from_input_to_hidden
+    def forward(self, x):
+        x = self.conv_layer(x)
+        x = x.view(-1, 20*20*32)
+        x = self.fc_layer(x)
+        return x
+
+
+# Instantiate the model
+model = ConvNet()
+# Define loss and optimizer
+criterion = nn.BCELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+#Train the Model
+
+n_epochs = 10
+
+for epoch in range(1, n_epochs+1):
+    train_losses = []
+    valid_losses = []
+    # Train the model
+    for batch, (inputs, labels) in enumerate(train_loader, 1):
+        # Clear the gradients
+        optimizer.zero_grad()
+        # Forward pass
+        outputs = model.forward(inputs)
+        # Calculate Loss
+        loss = criterion(outputs, labels)
+        # Backward pass
+        loss.backward()
+        # Update weights
+        optimizer.step()
+        # Store the loss
+        train_losses.append(loss.item())
+        # Validate Model
+    with torch.no_grad():
+        valid_loss = 0.0
+        for inputs, labels in validation_loader:
+            outputs = model.forward(inputs)
+            loss = criterion(outputs, labels)
+            valid_loss += loss.item()
+        valid_loss /= len(validation_loader)
+        valid_losses.append(valid_loss)
+    # Print training losses
+    print(f'Epoch: {epoch} \tTraining Loss: {np.mean(train_losses):.5f} \tValidation Loss: {np.mean(valid_losses):.5f}')
+
+#Test the Model
+
+test_losses = []
+
+#Test the model
+with torch.no_grad():
+    test_loss = 0.0
+    for inputs, labels in test_loader:
+        outputs = model.forward(inputs)
+        loss = criterion(outputs, labels)
+        test_loss += loss.item()
+    test_loss /= len(test_loader)
+    test_losses.append(test_loss)
+
+print(f'Test Loss: {np.mean(test_losses):.5f}')
+
+# Make Predictions
+
+# Make Predictions on the test set
+with torch.no_grad():
+    preds = []
+
+# Get the predicted values
+    for batch, (inputs, labels) in enumerate(test_loader, 1):
+        pred = model.forward(inputs)
+        preds.append(pred)
+
+# Flatten the list of predictions
+preds = [pred.data.numpy()[0] for pred in preds]
+true_labels = [label.data.numpy()[0] for label in test_loader.dataset.tensors[1]]
